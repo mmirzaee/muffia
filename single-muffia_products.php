f49268e6ac9519da195b1a8d33a26c6d5ff42a2e
Ali M.Mirzaee <me@alimrz.com>
2019-05-06 22:25:39 +0430

    ...

--- single-muffia_products.php
+++ single-muffia_products.php
@@ -3,8 +3,9 @@ get_header();
 ?>
 <?php
 $pdf_url = get_post_meta(get_the_ID(), 'muffia_product_spec', true);
-echo $pdf_url;
-echo apply_filters('the_content', '[embeddoc  url="' . $pdf_url . '" download="none"]');
+if ($pdf_url):
+    echo apply_filters('the_content', '[embeddoc  url="' . $pdf_url . '" download="none"]');
+endif;
 ?>
 <?php
 get_footer();

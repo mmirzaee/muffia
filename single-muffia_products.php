8edd06361435ce3be760e78a099067235e0d75cd
Ali M.Mirzaee <me@alimrz.com>
2019-05-06 21:22:02 +0430

    added content to products single

--- single-muffia_products.php
+++ single-muffia_products.php
@@ -1,9 +1,37 @@
 <?php
 get_header();
 ?>
+<div id="Content">
+    <div class="content_wrapper clearfix">
 
-test
+        <div class="sections_group">
 
+            <div class="entry-content" itemprop="mainContentOfPage">
+
+                <div class="section the_content has_content">
+                    <div class="section_wrapper">
+                        <div class="the_content_wrapper">
+                            <div class="vc_row wpb_row vc_row-fluid">
+                                <div class="wpb_column vc_column_container vc_col-sm-12">
+                                    <div class="vc_column-inner">
+                                        <div class="wpb_wrapper"><!-- vc_grid start -->
+                                            <?php the_content(); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+</div>
 <?php
 get_footer();
 ?>

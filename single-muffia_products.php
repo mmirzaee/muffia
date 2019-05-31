78d81ce2c6093bad95fc33bc916153f0a20e0ad6
Ali M.Mirzaee <me@alimrz.com>
2019-05-31 18:10:52 +0430

    ...

--- single-muffia_products.php
+++ single-muffia_products.php
@@ -1,6 +1,20 @@
 <?php
 get_header();
 ?>
+<style>
+    .ead-preview {
+        width: 50%;
+        padding: 30px;
+    }
+
+    @media (max-width: 575.98px) {
+        .ead-preview {
+            width: 100%;
+            padding: 30px;
+        }
+    }
+
+</style>
 <?php
 $pdf_url = get_post_meta(get_the_ID(), 'muffia_product_spec', true);
 if ($pdf_url):
